e7450b5ed1e753f8c67c52a8ca0beece82423d21
golic98 <00214917@uca.edu.sv>
2025-09-04 16:05:53 -0600

    ejercicio 2

---
+++ operaciones matematicas/ejercicio2.pl
@@ -0,0 +1,9 @@
+% Operadores Aritméticos
+% -  Ejercicio 2: Número par o impar - Crear predicado par_impar/1.
+
+par_impar(X) :- 
+    integer(X), 
+    (0 is X mod 2
+    -> write('par')
+    ;  write('impar')
+    )
